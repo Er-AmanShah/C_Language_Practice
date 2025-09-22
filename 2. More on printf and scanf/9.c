6dc448613a6d76a8aabf0ced21783f83de45b86a
Er-AmanShah <amamshah7271@gmail.com>
2025-09-22 17:39:18 +0530

    Assignment 2: 9.c Completed (Program to take input time as HH:MM  and output as HH hour and MM minutes.)

---
+++ 2. More on printf and scanf/9.c
@@ -0,0 +1,15 @@
+// Write a program to take date as an input in below given format and convert the date format and display the result as given below.
+
+/* User Input date format - "DD/MM/YYYY"  (22/09/2025)    */
+/* Output format - "Day - DD , Month - MM , Year - YYYY" (Day-22, Month-09, Year-2025)*/
+
+#include<stdio.h>
+int main()
+{
+    int date, month, year;
+    printf("Enter Date (DD/MM/YYYY):");
+    scanf("%d/%d/%d", &date, &month, &year);
+
+    printf("Day - %02d , Month - %02d , Year - %04d", date, month, year);
+    return 0;
+}
